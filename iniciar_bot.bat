7e01e67fae12473f2940756c99698aed2cf8f441
Johanren <jfrengifo04@misena.edu.co>
2025-04-30 20:07:30 -0500

    .bat inicio

---
+++ iniciar_bot.bat
@@ -0,0 +1,6 @@
+npx concurrently ^
+  "cd C:\xampp\htdocs\chatbot && node app.js" ^
+  "cd C:\xampp\htdocs\chatbot && node server.js" ^
+  "cd C:\xampp\htdocs\chat && node app.js" ^
+  "cd C:\xampp\htdocs\chat2 && node app.js" ^
+  "cd C:\xampp\htdocs\chat3 && node app.js"
